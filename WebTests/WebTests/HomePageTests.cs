49160ce18ccd2c8754da226f3f60106e74abce8c
Mark Arnold <mark.arnold@accesshq.com>
2022-03-03 09:55:47 +1000

    Add VerifySubmit test

--- WebTests/WebTests/HomePageTests.cs
+++ WebTests/WebTests/HomePageTests.cs
@@ -1,19 +1,34 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Threading;
 
 namespace WebTests
 {
     [TestClass]
     public class HomePageTests
     {
+        private IWebDriver driver;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://d18u5zoaatmpxx.cloudfront.net";
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            driver.Quit();
+        }
+
         [TestMethod]
         public void Playground_VerifyHomepage()
         {
-            var driver = new ChromeDriver();
-            driver.Manage().Window.Maximize();
-            driver.Url = "https://d18u5zoaatmpxx.cloudfront.net";
-
             var headerElements = driver.FindElements(By.TagName("h1"));
 
             IWebElement foundHeader = null;
@@ -27,8 +42,24 @@ namespace WebTests
             }
 
             Assert.IsTrue(foundHeader.Displayed);
+        }
 
-            driver.Quit();
+        [TestMethod]
+        public void Playground_VerifySubmitButton()
+        {
+            const string Name = "Mark";
+            //Arrange
+
+            // Act
+            driver.FindElement(By.Id("forename")).SendKeys(Name);
+            driver.FindElement(By.Id("submit")).Click();
+
+            // Assert
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(2));
+            IWebElement popupElement = driver.FindElement(By.ClassName("popup-message"));
+            wait.Until(d => popupElement.Displayed);
+
+            Assert.AreEqual(expected: $"Hello {Name}", actual: popupElement.Text);
         }
     }
 }
