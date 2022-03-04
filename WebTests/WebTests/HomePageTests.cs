d2482891689993199f332b9cb73cc64dab52ed55
Mark Arnold <mark.arnold@accesshq.com>
2022-03-04 10:00:59 +1000

    Remove unncessary using statements

--- WebTests/WebTests/HomePageTests.cs
+++ WebTests/WebTests/HomePageTests.cs
@@ -1,9 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Support.UI;
-using System;
-using System.Threading;
 
 namespace WebTests
 {
